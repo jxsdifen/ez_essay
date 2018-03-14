aae7c35c203a4e921b51698f9bb6b13083b756dd
sky <jxsdifens@163.com>
2018-03-14 16:42:00 +0800

    更新

---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Login from '@/views/Login'
+import NotFound from '@/views/404'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // },
+    {
+      path: '/login',name: 'Login',component: Login
+    },
+    {
+      path: '/404',name: 'NotFound',component: NotFound
+    }
+  ]
+})
